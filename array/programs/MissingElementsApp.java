18a40550e197d4fa11c97c394b89ef0fb0766999
gauravlagad96 <gauravlagad96@gmail.com>
2024-11-26 22:56:42 +0530

    v1.0.0 star pattern

--- array/programs/MissingElementsApp.java
+++ array/programs/MissingElementsApp.java
@@ -1,11 +1,13 @@
 package com.array.programs;
 /* 
-Write a program to enter the 5 values in ascending order and store in array and find out the
+Write a program to enter the 5 values in ascending order and store in array and find out the 
 missing element from array?
+
 a[0] a[1] a[2] a[3] a[4]
-1 	   5	9 	13 	  17
+1 	   5	9 	13 	 17
 Missing elements : output should like as
 2 3 4 6 7 8 10 11 12 14 15 16
+
 */
 
 import java.util.Scanner;
@@ -14,6 +16,7 @@ public class MissingElementsApp {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int[] arr = new int[5];
+		
 		System.out.println("Enter 5 elements in an array");
 		for (int i = 0; i < 5; i++)
 			arr[i] = sc.nextInt();
@@ -37,7 +40,9 @@ public class MissingElementsApp {
 			System.out.print(ele + " ");
 		
 		//Missing Elements logic.
-		for(int i=0;i<arr.length-1;i++) {}
+		for(int i=0;i<arr.length-1;i++) {
+			
+		}
 		
 	}
 
--- pattern/Pattern2.java
+++ pattern/Pattern2.java
@@ -9,7 +9,7 @@ package com.pattern;
    * *
    * 
  */
-public class Pattern1 {
+public class Pattern2 {
 	
 	public static void main(String[] args) {
 		
@@ -17,10 +17,10 @@ public class Pattern1 {
 			for(int j=1;j<=4;j++) {  //col  j=2
 				
 				if(i<=4 && i>=j) {   //row 1 to 4 
-					System.out.print("*" + " ");   
+					System.out.print(j + " ");   
 				}
 				if(i>4 && j<=8-i) {  // row 5 to 7
-					System.out.print( "*" + " ");  // * 
+					System.out.print( j + " ");  // * 
 				}
 			}
 			System.out.println();
